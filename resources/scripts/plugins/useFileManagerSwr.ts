a8364aec2bdb5cf1eedffc5552a630fce4a27300
Fabricio Andrade <support@skycloud.app.br>
2026-02-03 13:54:46 +0000

    feat: update useFileManagerSwr to support paginated directory responses

--- resources/scripts/plugins/useFileManagerSwr.ts
+++ resources/scripts/plugins/useFileManagerSwr.ts
@@ -1,15 +1,16 @@
 import useSWR from 'swr';
-import loadDirectory, { FileObject } from '@/api/server/files/loadDirectory';
+import loadDirectory, { FileObject, PaginatedDirectory } from '@/api/server/files/loadDirectory';
 import { cleanDirectoryPath } from '@/helpers';
 import { ServerContext } from '@/state/server';
 
-export const getDirectorySwrKey = (uuid: string, directory: string): string => `${uuid}:files:${directory}`;
+export const getDirectorySwrKey = (uuid: string, directory: string): string =>
+    `${uuid}:files:${directory}`;
 
 export default () => {
     const uuid = ServerContext.useStoreState((state) => state.server.data!.uuid);
     const directory = ServerContext.useStoreState((state) => state.files.directory);
 
-    return useSWR<FileObject[]>(
+    const swr = useSWR<PaginatedDirectory>(
         getDirectorySwrKey(uuid, directory),
         () => loadDirectory(uuid, cleanDirectoryPath(directory)),
         {
@@ -19,4 +20,12 @@ export default () => {
             errorRetryCount: 2,
         }
     );
+
+    return {
+        ...swr,
+
+        // 🔥 aqui está a mágica
+        data: swr.data?.data ?? [],
+        meta: swr.data?.meta,
+    };
 };
